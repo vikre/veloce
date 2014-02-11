2ad895b99096443cdb23fccb38cc461ba54ba444
Fredrik Soløy <fredrik.soloy@gmail.com>
2014-02-11 14:39:40 +0100

    Added ruby to travis

---
+++ veloce.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+require 'fileutils' #I know, no underscore is not ruby-like
+include FileUtils
+
+name = String.new("VELOCE")
+version = String.new("v0.0.1")
+
+puts name + " " + version
+
+#app = String.new("evernote")
+#system "brew cask search " + app
+
+
+#----------------------- HomeBrew -----------------------
+puts " "
+puts "==> Installing HomeBrew"
+puts " "
+#ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+#----------------------- HomeBrewing -----------------------
+puts " "
+puts "==> Installing HomeBrew"
+puts " "
+
