1a549c407a897bf7a64fd744f1959e3e504153c1
Fredrik Soløy <fredrik.soloy@gmail.com>
2014-02-11 15:20:52 +0100

    Added the rest of the logic to veloce

--- veloce.rb
+++ veloce.rb
@@ -8,18 +8,37 @@ version = String.new("v0.0.1")
 
 puts name + " " + version
 
-#app = String.new("evernote")
-#system "brew cask search " + app
-
-
 #----------------------- HomeBrew -----------------------
 puts " "
 puts "==> Installing HomeBrew"
 puts " "
-#ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 #----------------------- HomeBrewing -----------------------
 puts " "
 puts "==> Installing HomeBrew"
 puts " "
+f = File.open("brew.txt", "r")
+f.each_line do |line|
+  system "brew install " + line
+end
+f.close
 
+# PRINT out what is installed
+system "brew list"
+
+#----------------------- HomeBrewing -----------------------
+puts " "
+puts "==> Installing Brew Cask"
+puts " "
+f = File.open("cask.txt", "r")
+f.each_line do |line|
+  system "brew cask install " + line
+end
+f.close
+
+# Alfred Integration
+system "brew cask alfred"
+
+# PRINT out what is installed
+system "brew cask list"
